74a38175744c240887609ab6d21341d08e9cf22d
omkukresh_25 <oalzeg09@gmail.com>
2026-06-07 10:54:43 +0300

    lambdas fix

--- calculate_lambdas.cpp
+++ calculate_lambdas.cpp
@@ -77,7 +77,7 @@ float lambda(map<int, int> &observed, int window_size)
     float p_emp = 0;
     float diff;
 
-    for (int n = 1; n < max_n; n++)
+    for (int n = 1; n <= max_n; n++)
     {
         p_theor += log2(1.0 + 1.0 / (n * (n + 2.0)));
         p_emp += observed.count(n) ? (float)observed[n] / window_size : 0;
@@ -85,52 +85,19 @@ float lambda(map<int, int> &observed, int window_size)
 
         if(diff > max_diff) max_diff = diff;
     }
-
+    
     return max_diff;
-}
-
-void calculate_distances(vector<int> &numbers, string file_name,
-			 map<int, int> &observed, vector<float> &lambdas)
-{
-    for(int i = 1; i < UPPER_BOUND - LOWER_BOUND; i++){
-	vector<int> sub(numbers.begin(), numbers.begin() + i);
-	observed.clear();
-	observe_frequencies(sub, observed);
-	lambdas.push_back(lambda(observed, LOWER_BOUND + i));
-    }
-}
-
-void fit_power(vector<float>& y, string name) {
-    double S_X = 0, S_Y = 0, S_XX = 0, S_XY = 0;
-    int N = 0;
-    for (int i = 0; i < y.size(); i++) {
-        if (y[i] <= 0) continue;
-        double x = i + 1;
-        double X = log(x);
-        double Y = log(y[i]);
-        S_X += X;
-        S_Y += Y;
-        S_XX += X * X;
-        S_XY += X * Y;
-        N++;
-    }
-    double B = (N * S_XY - S_X * S_Y) / (N * S_XX - S_X * S_X);
-    double C = (S_Y - B * S_X) / N;
-    double A = exp(C);
-    cout << name << ": y = " << A << " * x^" << B << endl;
 }
 
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
         vector<int> nums;
         map<int, int> obs;
-        //vector<float> lmb;
         string name = argv[i];
 	cout << name << "..." << endl;
         name = name.substr(name.find_last_of('/') + 1);
         name = name.substr(0, name.find_last_of('.'));
         get_fraction_numbers(nums, argv[i]);
-        //calculate_distances(nums, argv[i], obs, lmb);
 	observe_frequencies(nums, obs);   
 	float lmb = lambda(obs, nums.size());
         cout << name << ": lambda = " << lmb << endl;
